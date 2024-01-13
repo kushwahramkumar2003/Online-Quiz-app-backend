d2f243e84e075cbf3672c13f304d20cdba23b46a
kushwahramkumar2003 <kushwahramkumar2003gmail.com>
2024-01-13 16:56:38 +0530

    Update sameSite attribute in cookieOptions

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -10,7 +10,7 @@ const config = require("../config/index.js");
 const cookieOptions = {
   expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
   httpOnly: true,
-  sameSite: "none",
+  sameSite: "None",
   secure: true,
 };
 
