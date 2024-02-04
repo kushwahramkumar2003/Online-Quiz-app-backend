2beb7330533e4c647bf4b3d78022812dc98ad369
kushwahramkumar2003 <kushwahramkumar2003@gmail.com>
2024-02-04 21:32:11 +0530

    DB changed

--- src/config/index.js
+++ src/config/index.js
@@ -3,7 +3,7 @@ dotenv.config();
 
 const config = {
   MONGODB_URL:
-    // process.env.MONGODB_URL ||
+    process.env.MONGODB_URL ||
     "mongodb://localhost:27017/quiz-app",
   PORT: process.env.PORT || 3001,
 
